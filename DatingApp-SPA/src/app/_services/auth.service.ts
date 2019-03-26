4b8999a82e0bea3d4890453077c3e5a49ade4620
Marcos de Souza <marcos.dev.ti@gmail.com>
2019-03-25 22:43:38 -0400

    End of Section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {map } from 'rxjs/operators'
+import {map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -16,6 +19,8 @@ constructor(private http: HttpClient) { }
           const user: any = result;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         })
       );
@@ -27,5 +32,10 @@ constructor(private http: HttpClient) { }
 
     logout() {
       localStorage.removeItem('token');
+    }
+
+    loggedIn() {
+      const token = localStorage.getItem('token');
+      return !this.jwtHelper.isTokenExpired(token);
     }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { AlertifyService } from './_services/alertify.service';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -22,11 +24,13 @@ import { ErrorInterceptorProvide } from './_services/error.interceptor';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvide
+    ErrorInterceptorProvide,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
